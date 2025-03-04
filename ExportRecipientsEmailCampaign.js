d97c4b230a17e123f058826581d7f46fa3261631
Enai24 <enai.ai2024@gmail.com>
2025-03-04 14:17:07 +0000

    Delete ExportRecipientsEmailCampaign.js

--- ExportRecipientsEmailCampaign.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const ExportRecipientsEmailCampaign = () => {
-  return <div>Export Recipients Email Campaign Page</div>;
-};
-
-export default ExportRecipientsEmailCampaign;
